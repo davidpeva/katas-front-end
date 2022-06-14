eb0848596b9947ce5e97014138d514c28f0f1803
davidpeva <davidpeva@gmail.com>
2022-06-13 22:02:06 -0500

    mas cosas de la clase

--- app-react-core/main.js
+++ app-react-core/main.js
@@ -1,25 +1,29 @@
+//Clase del 09-06-22
+
+//ACCEDO AL ROOT PERO LO GUARDO EN UNA VARIABLE CONST Y AL IGUAL ELEMENT PARA MANIPULAR EL DOM
 const root = document.getElementById("div-root");
 const element = document.createElement("div");
 
-// MANIPULACION DEL DOM
+// MANIPULACION DEL DOM COMO SE HACE DESDE JS
 
 // element.textContent = "Hello world DOM";
 // element.className = "container";
 // root.appendChild(element);
 
-/** APLICADO CON REACT */
+/** APLICADO CON REACT USANDO EL VIRTUAL DOM*/
 
+//ACA ESTOY CREANDO EL ELEMENTO AUN NO SE MUESTRA EN EL DOM
 const elementReact = React.createElement("span", {}, "Hello world con react");
 
 const secondElementReact = React.createElement("div", {
-  children: React.createElement(
+    children: React.createElement(
     "span",
     {
-      children: React.createElement(),
-      className: "span-style",
+        children: React.createElement(),
+        className: "span-style",
     },
     "Este es mi span dentro de mi div"
-  ),
+    ),
 });
 
 /*
@@ -27,4 +31,6 @@ const secondElementReact = React.createElement("div", {
  * SINTAXIS React.createElement(typo_de_elemento, { children },  valor)
  */
 
+//ESTE EL METODO ES LA ENTRADA PRINCIPAL DE NUESTRO PROYECTO
+//el primer parametro es el elemento que quiero crear, el segundo es donde lo quiero mostrar
 ReactDOM.render(secondElementReact, root);
---
+++ react-with-lifecycle/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Hoy veremos ciclo de vida y renderizado condicional
+      </header>
+    </div>
+  );
+}
+
+export default App;
